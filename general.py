0e22b05c1cbbd1de5ee4caacd655f09e02660d34
Karl T Debiec <karl.t.debiec@gmail.com>
2022-03-27 13:38:53 -0400

    wip

--- general.py
+++ general.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 #   Copyright (C) 2017-2020 Karl T Debiec
-#   All rights reserved.
-#
-#   This software may be modified and distributed under the terms of the
-#   BSD license. See the LICENSE file for details.
+#   All rights reserved. This software may be modified and distributed under
+#   the terms of the BSD license. See the LICENSE file for details.
 """General-purpose functions not tied to a particular project."""
 from subprocess import PIPE, Popen
 from typing import Iterable, Optional, Tuple
