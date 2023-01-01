d3fc8a9d3b80c0f5ea7de573625c91031682efca
Karl T Debiec <karl.t.debiec@gmail.com>
2023-01-01 14:16:02 -0500

    wip

--- validation.py
+++ validation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#  Copyright 2017-2022 Karl T Debiec
+#  Copyright 2017-2023 Karl T Debiec
 #  All rights reserved. This software may be modified and distributed under
 #  the terms of the BSD license. See the LICENSE file for details.
 """General-purpose validation functions not tied to a particular project."""
