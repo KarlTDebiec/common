7cab86b416fd44778f7e63adc14ca8d40ac5c752
Karl T Debiec <karl.t.debiec@gmail.com>
2023-05-13 13:00:52 -0400

    Shrink four-line header to two lines

--- validation.py
+++ validation.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python
-#  Copyright 2017-2023 Karl T Debiec
-#  All rights reserved. This software may be modified and distributed under
-#  the terms of the BSD license. See the LICENSE file for details.
+#  Copyright 2017-2023 Karl T Debiec. All rights reserved. This software may be modified
+#  and distributed under the terms of the BSD license. See the LICENSE file for details.
 """General-purpose validation functions not tied to a particular project."""
 from __future__ import annotations
 
